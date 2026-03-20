af8874e28b1036a6fbaf8918e0085fbcecd8643e
ArihantSagari <155970293+ArihantSagari@users.noreply.github.com>
2026-03-20 20:34:04 +0530

    main done
    main done

--- src/main/java/com/kabutar/KabutarBackendApplication.java
+++ src/main/java/com/kabutar/KabutarBackendApplication.java
@@ -2,13 +2,15 @@ package com.kabutar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-@EnableScheduling
-@SpringBootApplication
+
+@SpringBootApplication(
+    exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+    }
+)
 public class KabutarBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KabutarBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KabutarBackendApplication.class, args);
+    }
 }
